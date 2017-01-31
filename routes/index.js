4e9c606fb5e2d7ecb094b63fac274942ab696109
ranjitgorai <gorai.ranjit13@gmail.com>
2017-01-31 18:05:43 +0530

    final

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/multerproj', function(req, res, next) {
+  res.render('home');
 });
+
+router.post('/multerproj', upload.any(), function (req, res, next) {
+	console.log(req.files);
+	console.log(req.body);
+	res.send(req.files);
+
+  
+});
+
+
 
 module.exports = router;
